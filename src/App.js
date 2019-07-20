b8fc6697cac261d3f9799baec333b281afd4f21d
bpeterson1663 <bpeterson1663@gmail.com>
2019-07-20 15:37:43 -0500

    implement routes and basic authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Container from './components/Container';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Navigation from './components/Navigation';
+import AuthContext from './auth-context';
 function App() {
+  const [authStatus, setAuthStatus] = useState(false);
+  const [authToken, setAuthToken] = useState('')
+  const login = (data) => {
+    setAuthStatus(true);
+    setAuthToken(data.idToken)
+  };
   return (
-    <div className="App">
-        <Container />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <AuthContext.Provider value={{status: authStatus, authToken: authToken, login: login}}>
+          <Navigation/>
+          <Route path="/retroList" component={null} />
+          <Route path="/retro" component={Container} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" exact component={SignUp} />
+        </AuthContext.Provider>  
+      </div>
+  </BrowserRouter>
   );
 }
 
