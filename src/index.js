1ef2302687bd61669d9e290d03c931bc2fc088b8
Brady Peterson <brady.peterson@calabrio.com>
2019-07-23 14:50:15 -0500

    fix IE

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/ie11';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
