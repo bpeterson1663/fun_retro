c3fec902de308a3b04abb1d1d7de8325454135b4
Brady Peterson <brady.peterson@calabrio.com>
2019-07-23 15:21:08 -0500

    add ie11 version

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
