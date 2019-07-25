5fb155c1316d7630a42601578505eb9faebd527b
bpeterson1663 <bpeterson1663@gmail.com>
2019-07-24 19:41:49 -0500

    remove local-storage dependency

--- src/components/Retro/RetroContainer.js
+++ src/components/Retro/RetroContainer.js
@@ -1,18 +1,13 @@
 import React, {useState} from 'react';
 import RetroColumn from './RetroColumn';
-import ls from 'local-storage'
 import VoteContext from './vote-context';
 
 const RetroContainer = (props) => {
-    const [remaingVotes, setRemaingVotes] = useState(ls.get(props.match.params.id) || 6);
+    const [remaingVotes, setRemaingVotes] = useState(6);
     const retroId = props.match.params.id;
-    
-    const setVotes = (votes) => {
-        ls.set(retroId, votes);
-        setRemaingVotes(votes);
-    }
+
     return(
-        <VoteContext.Provider value={{votes: remaingVotes, setRemaingVotes: setVotes }}>
+        <VoteContext.Provider value={{votes: remaingVotes, setRemaingVotes: setRemaingVotes }}>
             Remaining Votes: {remaingVotes}
             <RetroColumn retroId={retroId} title="Keep Doing" columnName="keepDoing" />
             <RetroColumn retroId={retroId} title="Stop Doing" columnName="stopDoing" />
