5992d4f3c4b143362a05c91e4a0f9871d23dbec4
bpeterson1663 <bpeterson1663@gmail.com>
2019-07-28 22:06:59 -0500

    Make votes editable

--- src/components/Admin/AdminContainer.js
+++ src/components/Admin/AdminContainer.js
@@ -6,17 +6,28 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/DeleteForeverOutlined';
+import EditIcon from '@material-ui/icons/Edit';
 import LinearProgress from '@material-ui/core/LinearProgress/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles(theme => ({
     inputField: {
       margin: theme.spacing(2),
     },
     icon: {
-        margin: theme.spacing(1),
-        fontSize: 32,
-        cursor: "pointer",
-        float: "right"
+        marginLeft: "auto"
+    },
+    card: {
+        width: 550,
+        margin: '10px 30px'
+    },
+    cardHeader: {
+        padding: 0
     },
     placeholder: {
         height: 5
@@ -28,6 +39,10 @@ const useStyles = makeStyles(theme => ({
     columnInfo: {
         width: '75%',
         margin: 'auto'
+    },
+    form: {
+        width: 400,
+        margin: 'auto'
     }
 }));
 const AdminContainer = () => {
@@ -35,6 +50,7 @@ const AdminContainer = () => {
     const [startDateValue, setStartDateValue] = useState('');
     const [endDateValue, setEndDateValue] = useState('')
     const [isLoading, setIsLoading] = useState(true);
+    const [voteValue, setVoteValue] = useState(6);
 
     const auth = useContext(AuthContext);
     const classes = useStyles();
@@ -76,19 +92,20 @@ const AdminContainer = () => {
               startDate: startDateValue,
               endDate: endDateValue,
               userId: auth.userId,
+              numberOfVotes: voteValue,
               isActive: true
             })
           .then((res) =>{
             setNameValue('');
             setEndDateValue('');
             setStartDateValue('');
+            setVoteValue(6);
             dispatch({
                 type: 'ADD', 
                 payload: {
                     name: nameValue, 
                     endDate: endDateValue, 
-                    startDate: 
-                    startDateValue, 
+                    startDate: startDateValue, 
                     id: res.id
                 }
             });
@@ -107,22 +124,43 @@ const AdminContainer = () => {
     return (
         <Container>
             {isLoading ? <LinearProgress variant="query"/> : <div className={classes.placeholder}></div>}
-            <h1>Create New Retro</h1>
-            <form onSubmit={onSubmitHandler}>
-                <TextField required className={classes.inputField} type="text" label="Retro Name" onChange={(e) => setNameValue(e.target.value)}/>
-                <TextField required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="Start of Sprint" onChange={(e) => setStartDateValue(e.target.value)}/>
-                <TextField required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="End of Sprint" onChange={(e) => setEndDateValue(e.target.value)}/>
-                <Button type="submit" value="Submit" color="secondary" variant="contained" className={classes.submit}>Create</Button>
-            </form>
-            {retroList.length > 0 ? <h1>Retro List</h1> : null } 
-            {retroList.map((retro, i) => {
-               return <div className={classes.columnInfo} key={i}>
-                    <DeleteIcon className={classes.icon} onClick={handleRetroDelete.bind(this, retro.id)} color="secondary">Delete</DeleteIcon>
-                    {retro.name}<br/>
-                    {retro.startDate} through {retro.endDate}<br/>
-                    Retro Link: <a  rel="noopener noreferrer" target="_blank" href={"https://superfunretro.herokuapp.com/retro/"+retro.id}>https://superfunretro.herokuapp.com/retro/{retro.id}</a><br/>
-                </div>
-            })}
+            <Grid container justify="center" spacing={0}>
+                <Grid item>
+                    <h1>Create New Retro</h1>
+                    <form onSubmit={onSubmitHandler} className={classes.form}>
+                        <TextField required className={classes.inputField} type="text" label="Retro Name" onChange={(e) => setNameValue(e.target.value)}/>
+                        <TextField required className={classes.inputField} type="number" label="Votes Per Person" value={voteValue} onChange={(e) => setVoteValue(e.target.value)}/>
+                        <TextField required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="Start of Sprint" onChange={(e) => setStartDateValue(e.target.value)}/>
+                        <TextField required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="End of Sprint" onChange={(e) => setEndDateValue(e.target.value)}/>
+                        <Button type="submit" value="Submit" color="secondary" variant="contained" className={classes.submit}>Create</Button>
+                    </form>
+                </Grid>
+                <Grid item>
+                    {retroList.length > 0 ? <h1>Retro List</h1> : null } 
+                    {retroList.map((retro, i) => {
+                        return (
+                            <Card className={classes.card} key={i}>
+                                <CardHeader
+                                    className={classes.cardHeader}
+                                    title={retro.name}
+                                    />
+                                <CardContent>
+                                    {retro.startDate} through {retro.endDate}<br/>
+                                    Retro Link: <a  rel="noopener noreferrer" target="_blank" href={"https://superfunretro.herokuapp.com/retro/"+retro.id}>https://superfunretro.herokuapp.com/retro/{retro.id}</a><br/>
+                                </CardContent>
+                                <CardActions>
+                                    {/* <IconButton className={classes.icon}>
+                                        <EditIcon color="secondary" />
+                                    </IconButton> */}
+                                    <IconButton className={classes.icon} onClick={handleRetroDelete.bind(this, retro.id)}>
+                                        <DeleteIcon color="secondary">Delete</DeleteIcon>
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
+                        )
+                    })}
+                </Grid>
+            </Grid>
         </Container>
     );  
 };
