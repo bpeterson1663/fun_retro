97f11bc0f83a5022109b4c444dfbe9b85b1bfed7
bpeterson1663 <bpeterson1663@gmail.com>
2019-08-11 12:16:36 -0500

    update variables with staging environment

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,39 @@
 import firebase from "firebase";
 
+let apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId;
+
+if (process.env.NODE_ENV === 'production') {
+    apiKey = process.env.REACT_APP_API_KEY;
+    authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+    databaseURL = process.env.REACT_APP_DATABASE_URL;
+    projectId = process.env.REACT_APP_PROJECT_ID;
+    storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+    messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+    appId = process.env.REACT_APP_APP_ID;
+} else {
+    apiKey = process.env.REACT_APP_STAGE_API_KEY;
+    authDomain = process.env.REACT_APP_STAGE_AUTH_DOMAIN;
+    databaseURL = process.env.REACT_APP_STAGE_DATABASE_URL;
+    projectId = process.env.REACT_APP_STAGE_PROJECT_ID;
+    storageBucket = process.env.REACT_APP_STAGE_STORAGE_BUCKET;
+    messagingSenderId = process.env.REACT_APP_STAGE_MESSAGING_SENDER_ID;
+    appId = process.env.REACT_APP_STAGE_APP_ID;
+}
+
 const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
 });
 
 const db = firebaseApp.firestore();
