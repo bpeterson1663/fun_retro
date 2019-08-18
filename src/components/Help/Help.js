9575f2db9be4e9647059cb0f6cb7ad07816c42f8
bpeterson1663 <bpeterson1663@gmail.com>
2019-08-17 20:31:39 -0500

    Add more text to help

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -51,8 +51,9 @@ const Help = () => {
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                 <Typography>
-                    Using Super Fun Retro is easy! Simple create your retro and then send the link that is generated out to your team. Your team members then login and can start entering in feedback in three different categories; Keep Doing, Stop Doing, and Start Doing. 
+                    Using Super Fun Retro is easy! Simply create your retro and send the link that is generated out to your team. Your team members then login and can start entering in feedback in three different categories; Keep Doing, Stop Doing, and Start Doing. 
                     Each team member can then vote for any item added they like. All feedback is anonymous to help encourage a safe and free environment to provide more honest feedback. 
+                    Only the user who creates the Super Fun Retro will be able to acivate/disable the retro as well as edit, delete or generate a report. 
                 </Typography>
                 </ExpansionPanelDetails>
             </ExpansionPanel>
@@ -68,6 +69,20 @@ const Help = () => {
                 <Typography>
                     Absolutely nothing! It is free to use! No limits on how many people can sign up, no limits on how many retrospectives you can create, and no limits on how long you can use it! 
                     Other so called "Fun Retros" charge you after a certain time period, which isn't really fun in my opinion.
+                </Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                    >
+                <Typography className={classes.heading}>Does everyone have to have an account that uses Super Fun Retro?</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                <Typography>
+                    Yes anyone who uses Super Fun Retro will need to create an account. Signing up is easy and all you need is an email.
                 </Typography>
                 </ExpansionPanelDetails>
             </ExpansionPanel>
