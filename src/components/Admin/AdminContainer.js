f5e8ae62dc39ac85e87b5d88cd8dcdade7be3c07
bpeterson1663 <bpeterson1663@gmail.com>
2019-08-25 15:20:53 -0500

    fix bug where values were not being reset properly

--- src/components/Admin/AdminContainer.js
+++ src/components/Admin/AdminContainer.js
@@ -109,6 +109,7 @@ const AdminContainer = () => {
             setEndDateValue('');
             setStartDateValue('');
             setVoteValue(6);
+            debugger;
             dispatch({
                 type: 'ADD', 
                 payload: {
@@ -184,10 +185,10 @@ const AdminContainer = () => {
                 <Grid item>
                     <Typography variant="h3">Create New Retro</Typography>
                     <form onSubmit={onSubmitHandler} className={classes.form}>
-                        <TextField name="retro_name" required className={classes.inputField} type="text" label="Retro Name" onChange={(e) => setNameValue(e.target.value)}/>
+                        <TextField name="retro_name" required className={classes.inputField} type="text" label="Retro Name" value={nameValue} onChange={(e) => setNameValue(e.target.value)}/>
                         <TextField name="retro_vote" required className={classes.inputField} type="number" label="Votes Per Person" value={voteValue} onChange={(e) => setVoteValue(e.target.value)}/>
-                        <TextField name="retro_start" required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="Start of Sprint" onChange={(e) => setStartDateValue(e.target.value)}/>
-                        <TextField name="retro_end" required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="End of Sprint" onChange={(e) => setEndDateValue(e.target.value)}/>
+                        <TextField name="retro_start" required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="Start of Sprint" value={startDateValue} onChange={(e) => setStartDateValue(e.target.value)}/>
+                        <TextField name="retro_end" required className={classes.inputField} type="date" InputLabelProps={{ shrink: true }} label="End of Sprint" value={endDateValue} onChange={(e) => setEndDateValue(e.target.value)}/>
                         <Button type="submit" value="Submit" color="secondary" variant="contained" className={classes.submit}>Create</Button>
                     </form>
                 </Grid>
