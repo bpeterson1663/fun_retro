c9ed1d7154abb5b233804fd546e643862d0b3925
bpeterson1663 <bpeterson1663@gmail.com>
2019-11-10 22:36:56 -0600

    add routes to server

--- server/server.js
+++ server/server.js
@@ -3,10 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const db = require('./db');
+const retroRouter = require('./routes/retro.router');
+
 const PORT = 4000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(PORT, () => {
-    console.log("server runningo on port: " + PORT);
-});
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use('/api', retroRouter);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
