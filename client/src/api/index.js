eb5c9f3ba04b86fea8c5ad325d398d04c0827205
bpeterson1663 <bpeterson1663@gmail.com>
2019-11-23 17:39:37 -0600

    add item api calls to front end

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -10,12 +10,21 @@ export const updateRetroById = (id, payload) => api.put(`/retro/${id}`, payload)
 export const deleteRetroById = id => api.delete(`/retro/${id}`);
 export const getRetroById = id => api.get(`/retro/${id}`);
 
+export const createItem = payload => api.post(`/item`, payload);
+export const getAllItems = (retroId, columnName) => api.get(`/retro/${retroId}/items/${columnName}`);
+export const deleteItem = id => api.delete(`/item/${id}`);
+export const updateItem = (id, payload) => api.put(`/item/${id}`, payload);
+
 const apis = {
     createRetro,
     getAllRetros,
     updateRetroById,
     deleteRetroById,
-    getRetroById
+    getRetroById,
+    createItem,
+    getAllItems,
+    deleteItem,
+    updateItem
 };
 
 export default apis;
