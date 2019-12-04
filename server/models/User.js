32c56439c225f411cc224a5732080c1b7b7d1dcc
bpeterson1663 <bpeterson1663@gmail.com>
2019-12-04 15:28:56 -0600

    create user model

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+    {
+        email: {type: String, required: true},
+        password: { type: String, required: true}
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('users', User);
