f1e6d9ffca842259e1be0d6cfdb9252ce4e5d08c
bpeterson1663 <bpeterson1663@gmail.com>
2020-01-01 14:10:53 -0600

    add closeButton style

--- src/components/Admin/AdminContainer.styles.js
+++ src/components/Admin/AdminContainer.styles.js
@@ -41,7 +41,13 @@ const useStyles = makeStyles(theme => ({
     },
     tableContainer: {
         maxWidth: 1200
-    }
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+      }
 }));
 
 export default useStyles;
