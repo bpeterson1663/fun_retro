c4209e8d092a5c709b984a763008306ac4de54d0
Brady Peterson <bpeterson1663@gmail.com>
2020-04-12 21:09:40 -0500

    test(footer test): make sure footer test renders

---
+++ src/components/__tests__/Footer.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Footer from "../Footer";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+it("Footer Loads", () => {
+  render(<BrowserRouter><Footer /></BrowserRouter>);
+});
