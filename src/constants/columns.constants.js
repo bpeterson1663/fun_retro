8b2d1158d5f1032072dd64ed209fb30b4319124f
Brady Peterson <bpeterson1663@gmail.com>
2020-08-24 07:34:13 -0500

    feat(retro types): add more retro types

--- src/constants/columns.constants.js
+++ src/constants/columns.constants.js
@@ -1,24 +1,44 @@
+const $green = '#009588',
+    $red = '#E91D63',
+    $purple = '#9C28B0',
+    $blue = '#2196F3';
+
 export const columnKeys = {
   keepDoing: [
-    { title: 'Keep Doing', value: 'keepDoing', backgroundColor: '#009588' },
-    { title: 'Stop Doing', value: 'stopDoing', backgroundColor: '#E91D63' },
-    { title: 'Start Doing', value: 'startDoing', backgroundColor: '#9C28B0' },
+    { title: 'Keep Doing', value: 'keepDoing', backgroundColor: $green },
+    { title: 'Stop Doing', value: 'stopDoing', backgroundColor: $red },
+    { title: 'Start Doing', value: 'startDoing', backgroundColor: $purple },
   ],
   mad: [
-    { title: 'Mad', value: 'mad', backgroundColor: '#E91D63' },
-    { title: 'Sad', value: 'sad', backgroundColor: '#9C28B0' },
-    { title: 'Glad', value: 'glad', backgroundColor: '#009588' },
+    { title: 'Mad', value: 'mad', backgroundColor: $red },
+    { title: 'Sad', value: 'sad', backgroundColor: $purple },
+    { title: 'Glad', value: 'glad', backgroundColor: $green },
   ],
   liked: [
-    { title: 'Liked', value: 'liked', backgroundColor: '#009588' },
-    { title: 'Learned', value: 'learned', backgroundColor: '#9C28B0' },
-    { title: 'Lacked', value: 'lacked', backgroundColor: '#E91D63' },
-    { title: 'Longed For', value: 'longedFor', backgroundColor: '#2196f3' },
+    { title: 'Liked', value: 'liked', backgroundColor: $green },
+    { title: 'Learned', value: 'learned', backgroundColor: $purple },
+    { title: 'Lacked', value: 'lacked', backgroundColor: $red },
+    { title: 'Longed For', value: 'longedFor', backgroundColor: $blue },
   ],
   whatWentWell: [
-    { title: 'What Went Well', value: 'whatWentWell', backgroundColor: '#009588' },
-    { title: 'What Did Not Go Well', value: 'whatDidNotGoWell', backgroundColor: '#E91D63' },
+    { title: 'What Went Well', value: 'whatWentWell', backgroundColor: $green },
+    { title: 'What Did Not Go Well', value: 'whatDidNotGoWell', backgroundColor: $red },
   ],
+  keep: [
+    { title: 'Keep', value: 'keep', backgroundColor: $blue },
+    { title: 'Add', value: 'keep', backgroundColor: $purple },
+    { title: 'Less', value: 'less', backgroundColor: $red },
+    { title: 'More', value: 'more', backgroundColor: $green },
+  ],
+  toDo: [
+      {title: 'To Do', value: 'toDo', backgroundColor: $blue},
+      {title: 'Doing', value: 'doing', backgroundColor: $purple},
+      {title: 'Done', value: 'done', backgroundColor: $green}
+  ],
+  sailboat: [
+    { title: 'Anchors (What Weighed Us Down)', value: 'anchor', backgroundColor: $red },
+    { title: 'Gusts of Wind (What Pushed Us Forward)', value: 'gust', backgroundColor: $green },
+  ]
 }
 
 export const columnTitles = [
@@ -26,6 +46,9 @@ export const columnTitles = [
   { value: 'mad', title: 'Mad, Sad, Glad' },
   { value: 'liked', title: 'Liked, Learned, Lacked, Longed For' },
   { value: 'whatWentWell', title: 'What Went Well, What Did Not Go Well' },
+  { value: 'keep', title: 'Keep, Add, Less, More'},
+  { value: 'toDo', title: 'To Do, Doing, Done'},
+  { value: 'sailboat', title: 'Sailboat'}
 ]
 
 export const getColumnsTitle = key => {
