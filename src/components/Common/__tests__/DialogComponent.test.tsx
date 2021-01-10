212dcbd134acecbe2384c82e863199731543ab07
Brady Peterson <bpeterson1663@gmail.com>
2021-01-10 16:10:27 -0600

    test(dialog component): begin test coverage for dialog component

---
+++ src/components/Common/__tests__/DialogComponent.test.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { render, screen } from '@testing-library/react'
+import DialogComponent from '../DialogComponent'
+import '@testing-library/jest-dom'
+test('should render dialog', () => {
+    const { getByTestId } = render(
+        <DialogComponent
+            open={true}
+            onClose={() => {return false}}
+            title="Test Dialog"
+            actions={[
+                <button key={0}>Action</button>,
+                <button key={1}>Close</button>
+            ]}
+        >
+            Dialog Content 
+        </DialogComponent>
+    )
+    expect(getByTestId('dialog_component')).toBeTruthy()
+    expect(screen.getByText('Dialog Content')).toBeInTheDocument();
+
+})
