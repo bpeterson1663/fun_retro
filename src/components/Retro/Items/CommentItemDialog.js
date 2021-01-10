817cdcc874f5868868943385cc5154a1b828dad7
Brady Peterson <bpeterson1663@gmail.com>
2021-01-10 13:20:02 -0600

    refactor(dialog component): refactor components to use common dialog component

--- src/components/Retro/Items/CommentItemDialog.js
+++ src/components/Retro/Items/CommentItemDialog.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
+import DialogComponent from '../../Common/DialogComponent'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import useStyles from '../Retro.styles'
@@ -27,32 +21,28 @@ const CommentItemDialog = props => {
     setCommentValue('')
   }
   return (
-    <Dialog data-testid="comment_dialog" open={!!showCommentDialog.item} onClose={handleCommentClose}>
-      <DialogTitle>
-        <Typography>Add a constructive comment</Typography>
-        <IconButton className={classes.closeButton} onClick={handleCommentClose}>
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent className={classes.dialogContent}>
-        <TextField
-          variant="outlined"
-          multiline
-          rows="3"
-          maxLength="1000"
-          className={`${classes.inputField} ${classes.inputFieldText}`}
-          type="text"
-          label="Start Typing"
-          value={commentValue}
-          onChange={e => setCommentValue(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
+    <DialogComponent
+      open={!!showCommentDialog.item}
+      onClose={handleCommentClose}
+      title="Add a constructive comment"
+      actions={
         <Button disabled={!commentValue} onClick={onSubmitHandler} color="secondary" variant="contained">
           Add Comment
         </Button>
-      </DialogActions>
-    </Dialog>
+      }
+    >
+      <TextField
+        variant="outlined"
+        multiline
+        rows="3"
+        maxLength="1000"
+        className={`${classes.inputField} ${classes.inputFieldText}`}
+        type="text"
+        label="Start Typing"
+        value={commentValue}
+        onChange={e => setCommentValue(e.target.value)}
+      />
+    </DialogComponent>
   )
 }
 
--- src/components/Retro/Items/EditCommentDialog.js
+++ src/components/Retro/Items/EditCommentDialog.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
+import DialogComponent from '../../Common/DialogComponent'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import useStyles from '../Retro.styles'
@@ -32,32 +26,28 @@ const EditCommentDialog = props => {
     setCommentValue('')
   }
   return (
-    <Dialog data-testid="edit-comment_dialog" open={!!item} onClose={handleCommentClose}>
-      <DialogTitle>
-        <Typography>Edit comment</Typography>
-        <IconButton className={classes.closeButton} onClick={handleCommentClose}>
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent className={classes.dialogContent}>
-        <TextField
-          variant="outlined"
-          multiline
-          rows="3"
-          maxLength="1000"
-          className={`${classes.inputField} ${classes.inputFieldText}`}
-          type="text"
-          label="Keep Typing"
-          value={commentValue}
-          onChange={e => setCommentValue(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
+    <DialogComponent
+      open={!!item}
+      onClose={handleCommentClose}
+      title="Edit Comment"
+      actions={
         <Button disabled={!commentValue} onClick={onSubmitHandler} color="secondary" variant="contained">
           Edit Comment
         </Button>
-      </DialogActions>
-    </Dialog>
+      }
+    >
+      <TextField
+        variant="outlined"
+        multiline
+        rows="3"
+        maxLength="1000"
+        className={`${classes.inputField} ${classes.inputFieldText}`}
+        type="text"
+        label="Keep Typing"
+        value={commentValue}
+        onChange={e => setCommentValue(e.target.value)}
+      />
+    </DialogComponent>
   )
 }
 
