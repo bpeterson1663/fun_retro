699418de1584bc7350137e7472b9c8203ae0cdfa
Brady Peterson <bpeterson1663@gmail.com>
2021-01-14 22:02:05 -0600

    test(item): start test coverage for create/edit item

--- src/components/Retro/Items/__tests__/CreateItem.test.tsx
+++ src/components/Retro/Items/__tests__/CreateItem.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import CreateItem from '../CreateItem'
 import { render } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
 
 it('CreateItem Renders', () => {
-  render(<CreateItem columnName={'keepDoing'} isActive={true} itemSubmit={jest.fn()} />)
+  const columnType = 'keepDoing'
+  const { getByTestId } = render(<CreateItem columnName={columnType} isActive={true} itemSubmit={jest.fn()} />)
+  userEvent.type(getByTestId(`column-${columnType}-textfield`).querySelector('textarea'), 'Creating an Item')
+  expect(getByTestId(`column-${columnType}-textfield`).querySelector('textarea').value).toEqual('Creating an Item')
+  userEvent.click(getByTestId(`column-${columnType}-addButton`))
 })
