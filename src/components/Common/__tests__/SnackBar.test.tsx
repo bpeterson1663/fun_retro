8318786295c2dc1832b1e353643351f91710f187
Brady Peterson <bpeterson1663@gmail.com>
2021-01-31 22:41:44 -0600

    test(snack bar): start test coverage for snack bar

---
+++ src/components/Common/__tests__/SnackBar.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SnackBar from '../SnackBar'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+test('should render snack bar correctly', () => {
+    const { getByText } = render(<SnackBar open={true} onClose={() => {return}} message="This was a success" status="success" />)
+    expect(getByText('This was a success')).toBeInTheDocument()
+})
