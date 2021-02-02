2fd37a271b11ad88aa9fd379257e7dcaa0556077
Brady Peterson <bpeterson1663@gmail.com>
2021-02-01 22:54:40 -0600

    style(prettier): ran prettier

--- src/components/Common/__tests__/SnackBar.test.tsx
+++ src/components/Common/__tests__/SnackBar.test.tsx
@@ -4,6 +4,15 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 test('should render snack bar correctly', () => {
-    const { getByText } = render(<SnackBar open={true} onClose={() => {return}} message="This was a success" status="success" />)
-    expect(getByText('This was a success')).toBeInTheDocument()
+  const { getByText } = render(
+    <SnackBar
+      open={true}
+      onClose={() => {
+        return
+      }}
+      message="This was a success"
+      status="success"
+    />,
+  )
+  expect(getByText('This was a success')).toBeInTheDocument()
 })
