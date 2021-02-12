a8867d4d41be343fcf3e58e1add5a508ff37f28e
Brady Peterson <bpeterson1663@gmail.com>
2021-02-12 07:52:33 -0600

    fix(material ui): set index for custom styles to set precedence

--- src/components/Common/Common.styles.tsx
+++ src/components/Common/Common.styles.tsx
@@ -1,61 +1,64 @@
 import { makeStyles } from '@material-ui/core/styles'
 import { green } from '@material-ui/core/colors'
 
-const useStyles = makeStyles(theme => ({
-  footer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    height: 50,
-    position: 'fixed',
-    bottom: 0,
-    marginTop: 40,
-    width: '100%',
-    backgroundColor: theme.palette.primary.main,
-  },
-  copyright: {
-    color: '#fff',
-    maxWidth: 300,
-    '&:hover': {
-      cursor: 'auto',
+const useStyles = makeStyles(
+  theme => ({
+    footer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      height: 50,
+      position: 'fixed',
+      bottom: 0,
+      marginTop: 40,
+      width: '100%',
+      backgroundColor: theme.palette.primary.main,
     },
-  },
-  faqButton: {
-    color: '#fff',
-    '&:hover': {
-      cursor: 'auto',
+    copyright: {
+      color: '#fff',
+      maxWidth: 300,
+      '&:hover': {
+        cursor: 'auto',
+      },
     },
-  },
-  faq: {
-    textDecoration: 'none',
-    color: '#fff',
-  },
-  button: {
-    margin: theme.spacing(1),
-    color: '#fff',
-  },
-  header: {
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    margin: '0 0 0 auto',
-  },
-  dialogContent: {
-    width: '90%',
-    margin: 'auto',
-    display: 'flex',
-    flexFlow: 'row wrap',
-    justifyContent: 'center',
-  },
-  error: {
-    backgroundColor: theme.palette.error.dark,
-  },
-  success: {
-    backgroundColor: green[600],
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-}))
+    faqButton: {
+      color: '#fff',
+      '&:hover': {
+        cursor: 'auto',
+      },
+    },
+    faq: {
+      textDecoration: 'none',
+      color: '#fff',
+    },
+    button: {
+      margin: theme.spacing(1),
+      color: '#fff',
+    },
+    header: {
+      fontWeight: 'bold',
+    },
+    buttonContainer: {
+      margin: '0 0 0 auto',
+    },
+    dialogContent: {
+      width: '90%',
+      margin: 'auto',
+      display: 'flex',
+      flexFlow: 'row wrap',
+      justifyContent: 'center',
+    },
+    error: {
+      backgroundColor: theme.palette.error.dark,
+    },
+    success: {
+      backgroundColor: green[600],
+    },
+    message: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+  }),
+  { index: 1 },
+)
 
 export default useStyles
